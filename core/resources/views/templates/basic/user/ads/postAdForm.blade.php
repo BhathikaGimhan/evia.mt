4cf9dd203de098ca878690693625c0b8cf3def25
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-08-16 23:03:50 +0530

    fix

--- core/resources/views/templates/basic/user/ads/postAdForm.blade.php
+++ core/resources/views/templates/basic/user/ads/postAdForm.blade.php
@@ -27,7 +27,7 @@
                  <button type="button" class="imgRemove" onclick="myImgRemove(1)"></button>
                </label>
                <input type="file"  name="prev_image" id="file-ip-1" accept="image/*" onchange="showPreview(event, 1);">
-               <code>thumbnail image</code>
+               <code style="font-size: 10px;">thumbnail image</code>
              </div>
            </div>
          </div><!-- single-upload end -->
